e5ee8f71c614d0371065d9c828243fe5f9fb3636
Tomnguyen2604 <63090801+Tomnguyen2604@users.noreply.github.com>
2021-02-07 17:25:29 -0700

    Adding bot

---
+++ wednesday.py
@@ -0,0 +1,181 @@
+import os
+import playsound
+import speech_recognition as sr
+import time
+import sys
+import ctypes
+import wikipedia
+import datetime
+import json
+import re
+import webbrowser
+import smtplib
+import requests
+import urllib
+import urllib.request as urllib2
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from time import strftime
+from gtts import gTTS
+from youtube_search import YoutubeSearch
+#default
+wikipedia.set_lang('vi')
+language = 'vi'
+path = ChromeDriverManager().install()
+#Text into sound
+def speak(text):
+    print("Bot: {}".format(text))
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save("sound.mp3")
+    playsound.playsound("sound.mp3", False)
+    os.remove("sound.mp3")
+#Sound into text
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Tôi: ", end='')
+        audio = r.listen(source, phrase_time_limit=5)
+        try:
+            text = r.recognize_google(audio, language="vi-VN")
+            print(text)
+            return text
+        except:
+            print("...")
+            return 0
+def stop():
+    speak("Hẹn gặp lại bạn sau!")
+
+def get_text():
+    for i in range(3):
+        text = get_audio()
+        if text:
+            return text.lower()
+        elif i < 2:
+            speak("Bot không nghe rõ. Bạn nói lại được không!")
+    time.sleep(2)
+    stop()
+    return 0
+
+
+#name
+def hello(name):
+    day_time = int(strftime('%H'))
+    if day_time < 12:
+        speak("Chào buổi sáng bạn {}. Chúc bạn một ngày tốt lành.".format(name))
+    elif 12 <= day_time < 18:
+        speak("Chào buổi chiều bạn {}. Bạn đã dự định gì cho chiều nay chưa.".format(name))
+    else:
+        speak("Chào buổi tối bạn {}. Bạn đã ăn tối chưa nhỉ.".format(name))
+#time_showing
+def get_time(text):
+    now = datetime.datetime.now()
+    if "giờ" in text:
+        speak('Bây giờ là %d giờ %d phút' % (now.hour, now.minute))
+    elif "ngày" in text:
+        speak("Hôm nay là ngày %d tháng %d năm %d" %
+              (now.day, now.month, now.year))
+    else:
+        speak("Bot chưa hiểu ý của bạn. Bạn nói lại được không?")   
+
+#Open application
+def open_application(text):
+    if "google" in text:
+        speak("Mở Google Chrome")
+        os.startfile('C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
+    elif "word" in text:
+        speak("Mở Microsoft Word")
+        os.startfile('C:\Program Files\Microsoft Office\\root\Office16\\WINWORD.EXE')
+    elif "excel" in text:
+        speak("Mở Microsoft Excel")
+        os.startfile('C:\Program Files\Microsoft Office\\root\Office16\EXCEL.EXE')
+    else:
+        speak("Ứng dụng chưa được cài đặt. Bạn hãy thử lại!")
+
+def open_website(text):
+    reg_ex = re.search('mở (.+)', text)
+    if reg_ex:
+        domain = reg_ex.group(1)
+        url = 'https://www.' + domain
+        webbrowser.open(url)
+        speak("Trang web bạn yêu cầu đã được mở.")
+        return True
+    else:
+        return False
+
+def open_google_and_search(text):
+    search_for = text.split("kiếm", 1)[1]
+    speak('Okay!')
+    driver = webdriver.Chrome(path)
+    driver.get("http://www.google.com")
+    que = driver.find_element_by_xpath("//input[@name='q']")
+    que.send_keys(str(search_for))
+    que.send_keys(Keys.RETURN)
+#senting email
+def send_email(text):
+    speak('Bạn gửi email cho ai nhỉ')
+    recipient = get_text()
+    if 'yến' in recipient:
+        speak('Nội dung bạn muốn gửi là gì')
+        content = get_text()
+        mail = smtplib.SMTP('smtp.gmail.com', 587)
+        mail.ehlo()
+        mail.starttls()
+        mail.login('tomnguyen2604@gmail.com', 'Monokuma007')
+        mail.sendmail('',
+                      '', content.encode('utf-8'))
+        mail.close()
+        speak('Email của bạn vùa được gửi. Bạn check lại email nhé hihi.')
+    else:
+        speak('Bot không hiểu bạn muốn gửi email cho ai. Bạn nói lại được không?')
+
+#checking weather
+def current_weather():
+    speak("Bạn muốn xem thời tiết ở đâu ạ.")
+    ow_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city = get_text()
+    if not city:
+        pass
+    api_key = "fe8d8c65cf345889139d8e545f57819a"
+    call_url = ow_url + "appid=" + api_key + "&q=" + city + "&units=metric"
+    response = requests.get(call_url)
+    data = response.json()
+    if data["cod"] != "404":
+        city_res = data["main"]
+        current_temperature = city_res["temp"]
+        current_pressure = city_res["pressure"]
+        current_humidity = city_res["humidity"]
+        suntime = data["sys"]
+        sunrise = datetime.datetime.fromtimestamp(suntime["sunrise"])
+        sunset = datetime.datetime.fromtimestamp(suntime["sunset"])
+        wthr = data["weather"]
+        weather_description = wthr[0]["description"]
+        now = datetime.datetime.now()
+        content = """
+        Hôm nay là ngày {day} tháng {month} năm {year}
+        Mặt trời mọc vào {hourrise} giờ {minrise} phút
+        Mặt trời lặn vào {hourset} giờ {minset} phút
+        Nhiệt độ trung bình là {temp} độ C
+        Áp suất không khí là {pressure} héc tơ Pascal
+        Độ ẩm là {humidity}%
+        Trời hôm nay quang mây. Dự báo mưa rải rác ở một số nơi.""".format(day = now.day,month = now.month, year= now.year, hourrise = sunrise.hour, minrise = sunrise.minute,
+                                                                           hourset = sunset.hour, minset = sunset.minute, 
+                                                                           temp = current_temperature, pressure = current_pressure, humidity = current_humidity)
+        speak(content)
+        time.sleep(20)
+    else:
+        speak("Không tìm thấy địa chỉ của bạn")
+#choosing songs
+def play_song():
+    speak('Xin mời bạn chọn tên bài hát')
+    mysong = get_text()
+    while True:
+        result = YoutubeSearch(mysong, max_results=10).to_dict()
+        if result:
+            break
+    url = 'https://www.youtube.com' + result[0]['channel_link']
+    webbrowser.open(url)
+    speak("Bài hát bạn yêu cầu đã được mở.")
+
+#
+
